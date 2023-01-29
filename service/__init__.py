7893bdd8df6717219bd7baa1e01a17a3b313a8ee
SoyDamianFlores <yoman.damian@gmail.com>
2023-01-29 09:12:28 -0500

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -7,6 +7,7 @@ and SQL database
 import sys
 from flask import Flask
 from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
@@ -16,6 +17,7 @@ app.config.from_object(config)
 
 # Import the routes After the Flask app is created
 talisman = Talisman(app)
+CORS(app)
 
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
